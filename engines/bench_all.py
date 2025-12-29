632260d4936eb08b796c9d0340b4af4d10cff698
Andrew Grant <andrew.github@grantnet.us>
2025-12-29 12:24:20 -0500

    Select engines for bench_all

--- engines/bench_all.py
+++ engines/bench_all.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import re
+
 from bench_engine import bench_engine
 
 def main(seconds, threads, debug):
@@ -9,6 +11,10 @@ def main(seconds, threads, debug):
     for name in sorted(os.listdir('tarballs')):
 
         engine = name.removesuffix('.tar.zst').replace('openrank-', '')
+
+        if args.regex and not re.match(args.regex, engine):
+            continue
+
         values = bench_engine(engine, seconds, threads, debug)
         avg    = sum(values) / len(values)
 
@@ -20,6 +26,7 @@ if __name__ == '__main__':
     p.add_argument('--seconds', type=int, default=5)
     p.add_argument('--threads', type=int, default=1)
     p.add_argument('--debug', default=False, action='store_true')
+    p.add_argument('--regex', default=None)
     args = p.parse_args()
 
     main(args.seconds, args.threads, args.debug)
